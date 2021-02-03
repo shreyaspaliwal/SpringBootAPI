b3db1c142c9b1f2bfc1f5d64ac470160a2aa55c1
shreyaspaliwal <shreyaspaliwal21@gmail.com>
2021-02-03 10:07:48 +0530

    New resource using POST

--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -2,18 +2,19 @@ package com.example.demo.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService
 {
-    private List<Topic> topics =  Arrays.asList
+    private List<Topic> topics =  new ArrayList<>( Arrays.asList
             (
                 new Topic("spring","Spring Framework","Spring framework description"),
                 new Topic("springboot","Spring Boot","Spring Boot description"),
                 new Topic("flask","Python Flask Framework","Python Flask web framework description")
-            );
+            ));
 
     public List<Topic> getAllTopics()
     {
@@ -23,5 +24,10 @@ public class TopicService
     public Topic getTopic(String id)
     {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic)
+    {
+        topics.add(topic);
     }
 }
