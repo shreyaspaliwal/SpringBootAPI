f96c2e7a95a909c5059d5cf7b9e90011cdfe40f5
shreyaspaliwal <shreyaspaliwal21@gmail.com>
2021-02-03 23:13:00 +0530

    Making Crud Operations with Repository

--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.example.demo.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,43 +10,33 @@ import java.util.List;
 @Service
 public class TopicService
 {
-    private List<Topic> topics =  new ArrayList<>( Arrays.asList
-            (
-                new Topic("spring","Spring Framework","Spring framework description"),
-                new Topic("springboot","Spring Boot","Spring Boot description"),
-                new Topic("flask","Python Flask Framework","Python Flask web framework description")
-            ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics()
     {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id)
     {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).orElse(null);
     }
 
     public void addTopic(Topic topic)
     {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic)
     {
-        for (int i = 0; i < topics.size(); i++)
-        {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id))
-            {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id)
     {
-        topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
